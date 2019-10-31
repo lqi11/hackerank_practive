366132fbd9549ef2fdeb65ac05efa00dbfb8118b
lin <42823286+lqi11@users.noreply.github.com>
2019-10-30 22:09:07 -0500

    3

---
+++ 006. Collections deque().py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+
+d = deque()
+for _ in range(int(input())): # n = int(input())
+    line = input().split()
+    if line[0] == 'append':
+        d.append(line[1])
+    elif line[0] == 'pop':
+        d.pop()
+    elif line[0] == 'popleft':
+        d.popleft()
+    elif line[0] == 'appendleft':
+        d.appendleft(line[1])
+print(d)
